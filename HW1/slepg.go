6d545576a72146415bbc51e7138f2ee8d77ef3da
有耶无耶 <1975402075@qq.com>
2017-10-22 12:50:47 +0800

    Update slepg.go

--- HW1/slepg.go
+++ HW1/slepg.go
@@ -15,14 +15,41 @@ type selpg_args struct {
 	line        int
 	find        bool
 	destination string
+	inFile      string
+}
+
+var choice string
+
+func StartWord()
+{
+	fmt.Printf("\nUsage: %s -s(start) -e(end) [ -f | -l(line) ][filename]\n",choice)
+}
+
+func situation_args(sa *selpg_args)
+{
+	if len(os.Args)<3 || (os.Args[1][0] != '-' || os.Args[1][1] != 's')
+	|| (os.Args[2][0] != '-' || os.Args[2][1] != 'e') || (sa.start > sa.end || sa.start <0 ||sa.end <0) 
+	{
+		fmt.Printf("Wrong input! Try again")
+	}
+	if sa.find == false
+	{
+		if sa.line < 0
+		{
+			fmt.Printf("Wrong input! Try again")
+		}
+		else if sa.line == -1
+		{
+			sa.line = 72
+		}
+	}
 }
 
 func main() {
 	var sa selpg_args
-	flag.Usage = func() {
-		fmt.Printf("Usage of %s: \n", os.Args[0])
-		flag.PrintDefaults()
-	}
+	choice = os.Args[0]
+	
+	flag.Usage = StartWord
 	flag.IntVar(&sa.start, "s", 0, "The start page")
 	flag.IntVar(&sa.end, "e", 0, "The end page")
 	flag.IntVar(&sa.line, "l", 72, "Lines per page")
